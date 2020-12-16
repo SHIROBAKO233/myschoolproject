c29dc8fae5a0cca1a7725ad56e1a31c6e1f09e5b
Borna C <451858251@qq.com>
2020-12-16 15:04:56 +0800

    第一次尝试性提交

--- src/main/java/com/cloudwise/myproject/MyprojectApplication.java
+++ src/main/java/com/cloudwise/myproject/MyprojectApplication.java
@@ -2,7 +2,13 @@ package com.cloudwise.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @create by: Borna 陈张恒
+ * @description: 主类
+ * @create time: 2020/12/16 14:47
+ * @param
+ * @return 
+ */
 @SpringBootApplication
 public class MyprojectApplication {
 
